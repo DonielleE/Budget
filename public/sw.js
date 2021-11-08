1f14e4e3653e9e2545b0ead018062f68f7c0d7be
Donielle  Edmondson <donielleedmondson@gmail.com>
2021-11-08 18:42:46 -0500

    update

--- public/sw.js
+++ public/sw.js
@@ -1,11 +1,60 @@
-const { Workbox } = require("workbox-window");
+var CACHE_NAME = "my-site-cache-v1";
+const DATA_CACHE_NAME = "data-cache-v1";
 
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
+var urlsToCache = [
+  "/",
+  "/db.js",
+  "/index.js",
+  "/manifest.json",
+  "/styles.css",
+  "/icons/icon-192x192.png",
+  "/icons/icon-512x512.png"
+];
 
-workbox.routing.registerRoute(
+self.addEventListener("install", function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
 
-)
+self.addEventListener("fetch", function(event) {
+  // cache all get requests to /api routes
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
 
-// i was trying to use the workbox however i was reading that the SW doesnt work without a bundle like webkit
- //i was trying so hard to learn how to use webkit because i know i am going to need it for my project 
- //it was not working at all 
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
+    );
+
+    return;
+  }
+
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request).then(function(response) {
+        if (response) {
+          return response;
+        } else if (event.request.headers.get("accept").includes("text/html")) {
+          // return the cached home page for all requests for html pages
+          return caches.match("/");
+        }
+      });
+    })
+  );
+});
